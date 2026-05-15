58db29ed83fe1f73fb21f95b13acd07b43554f43
terribleperson123 <munchyidiol69@gmail.com>
2026-05-15 17:39:52 -0500

    Testing temperory sleep. I will definitely remove it now.

--- include/kernel_attrs.h
+++ include/kernel_attrs.h
@@ -1,14 +1,33 @@
 #ifndef KERNEL_ATTRS_H
 #define KERNEL_ATTRS_H
-
 #if defined(__GNUC__) || defined(__clang__)
-   #define NO_VECTORIZE      __attribute__((optimize("no-tree-vectorize")))
-   #define GENERAL_REGS_ONLY __attribute__((target("general-regs-only")))
-   #define KERNEL_FUNC       GENERAL_REGS_ONLY NO_VECTORIZE
-#else
-   #define NO_VECTORIZE
-   #define GENERAL_REGS_ONLY
-   #define KERNEL_FUNC
-#endif
 
+#define ATTR(...) __attribute__((__VA_ARGS__))
+
+#define KERNEL_FUNCTION \
+    ATTR(target("general-regs-only"), optimize("no-tree-vectorize"))
+
+#define KERNEL_SLOW_FUNCTION \
+    ATTR(target("general-regs-only"), optimize("O0,no-tree-vectorize"), noinline)
+
+#define KERNEL_NO_INLINE  ATTR(noinline)
+#define KERNEL_USED       ATTR(used)
+#define KERNEL_UNUSED     ATTR(unused)
+#define KERNEL_PACKED     ATTR(packed)
+#define KERNEL_ALIGNED(x) ATTR(aligned(x))
+#define KERNEL_INLINE     inline
+
+#else
+
+#define ATTR(...)
+#define KERNEL_FUNCTION
+#define KERNEL_SLOW_FUNCTION
+#define KERNEL_NO_INLINE
+#define KERNEL_USED
+#define KERNEL_UNUSED
+#define KERNEL_PACKED
+#define KERNEL_ALIGNED(x)
+#define KERNEL_INLINE inline
+
+#endif
 #endif
--- kernel/main.c
+++ kernel/main.c
@@ -1,29 +1,62 @@
 #include "stdint.h"
 #include "kernel_attrs.h"
 #define VGA_LEN 4000
+
+//vga
 volatile char* vga_memory = (volatile char*)0xb8000;
 uint16_t g_vgacurrent_pointer = 0;
 
-static KERNEL_FUNC void vga_clear();
-static KERNEL_FUNC void vga_print(const char* str);
+//noinline ONLY for now
+KERNEL_NO_INLINE KERNEL_FUNCTION void vga_clear();
+KERNEL_NO_INLINE KERNEL_FUNCTION void vga_print(const char* str);
+KERNEL_SLOW_FUNCTION void _t_wait(uint64_t); //temperory
 
-
-
-int KERNEL_FUNC main()
+//KERNEL MAIN
+KERNEL_FUNCTION int main()
 {
    vga_clear();
-   vga_print("HELLO there\nFrom kernel main!");
+   vga_print("HELLO there!\n");
+   _t_wait(4000);
+
+   vga_print("From kernel main!\n");
+   _t_wait(5000);
+
+   vga_print("TODO: Make IDT to handle pg faults.\n");
+   _t_wait(5000);
+
+   vga_print("Then we page fault on purpose!\n");
+   _t_wait(7000);
+
+
+   vga_clear();
+
+   vga_print("Welp, we halt now!\n");
+   _t_wait(5000);
+   vga_clear();
+   int x = 0;
+   while(1)
+   {
+      for(int i = 0; i < x; i++)
+      {
+         vga_print(".");
+         _t_wait(300 + x);
+      }
+      vga_clear();
+      x++;
+   }
    __asm__ volatile ("hlt");
    return 1;
 }
 
-static KERNEL_FUNC void vga_clear()
+//CLEAR SCREEN
+KERNEL_NO_INLINE KERNEL_FUNCTION void vga_clear()
 {
    for(int i = 0; i < VGA_LEN; i++)
       vga_memory[i] = 0x00;
+   g_vgacurrent_pointer = 0;
 }
-
-static KERNEL_FUNC void vga_print(const char* str)
+//PRINT SCREEN
+KERNEL_NO_INLINE KERNEL_FUNCTION void vga_print(const char* str)
 {
    for(; *str != '\0'; str++)
    {
@@ -38,4 +71,17 @@ static KERNEL_FUNC void vga_print(const char* str)
          g_vgacurrent_pointer++;
       }
    }
+}
+
+//TEMPERORY WAIT
+//figured out that it takes around 3 seconds 
+//so div three and div 1000 to get milisecond worth of
+//addition
+#define _T_WAIT_UMILLIS 0x00000000FFFFFFFF/3/1000
+KERNEL_SLOW_FUNCTION void _t_wait(uint64_t millis)
+{
+    for(uint64_t i = 0; i < _T_WAIT_UMILLIS * millis; i++)
+    {
+      
+    }
 }
