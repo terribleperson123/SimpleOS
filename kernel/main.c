241f7bed12d279daf39828cbfd50d7167086c4d4
terribleperson123 <munchyidiol69@gmail.com>
2026-05-15 17:53:24 -0500

    incase

--- kernel/main.c
+++ kernel/main.c
@@ -14,36 +14,11 @@ KERNEL_SLOW_FUNCTION void _t_wait(uint64_t); //temperory
 //KERNEL MAIN
 KERNEL_FUNCTION int main()
 {
+
    vga_clear();
    vga_print("HELLO there!\n");
-   _t_wait(4000);
-
    vga_print("From kernel main!\n");
-   _t_wait(5000);
 
-   vga_print("TODO: Make IDT to handle pg faults.\n");
-   _t_wait(5000);
-
-   vga_print("Then we page fault on purpose!\n");
-   _t_wait(7000);
-
-
-   vga_clear();
-
-   vga_print("Welp, we halt now!\n");
-   _t_wait(5000);
-   vga_clear();
-   int x = 0;
-   while(1)
-   {
-      for(int i = 0; i < x; i++)
-      {
-         vga_print(".");
-         _t_wait(300 + x);
-      }
-      vga_clear();
-      x++;
-   }
    __asm__ volatile ("hlt");
    return 1;
 }
@@ -73,15 +48,3 @@ KERNEL_NO_INLINE KERNEL_FUNCTION void vga_print(const char* str)
    }
 }
 
-//TEMPERORY WAIT
-//figured out that it takes around 3 seconds 
-//so div three and div 1000 to get milisecond worth of
-//addition
-#define _T_WAIT_UMILLIS 0x00000000FFFFFFFF/3/1000
-KERNEL_SLOW_FUNCTION void _t_wait(uint64_t millis)
-{
-    for(uint64_t i = 0; i < _T_WAIT_UMILLIS * millis; i++)
-    {
-      
-    }
-}
